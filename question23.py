fd4a307147c4d0b65f8fbfa92fd8b1720cccb58a
Vuyisile Ndlovu <terrameijar@gmail.com>
2016-12-26 18:34:53 +0200

    We are back to easy stuff: operators

---
+++ question23.py
@@ -0,0 +1,16 @@
+'''
+Question 23
+level 1
+
+Question:
+    Write a method which can calculate square value of number
+
+Hints:
+    Using the ** operator
+'''
+def square(n):
+    n = n
+    return n ** 2
+
+print square(5)
+print square(10)
