2eb62562f57935ed23b78fac1af350b211db08df
Vuyisile Ndlovu <terrameijar@gmail.com>
2016-12-28 14:02:26 +0200

    Interesting, list slicing

---
+++ question34.py
@@ -0,0 +1,21 @@
+'''
+Question:
+Define a function which can generate a list where the values are
+square of numbers between 1 and 20 (both included).
+Then the function needs to print the first 5 elements in the list.
+
+Hints:
+
+Use ** operator to get power of a number.
+Use range() for loops.
+Use list.append() to add values into a list.
+Use [n1:n2] to slice a list
+'''
+
+def list_print():
+    list_ = list()
+    for item in range(1, 21):
+        list_.append(item ** 2)
+    return list_[:5]
+
+print list_print()
