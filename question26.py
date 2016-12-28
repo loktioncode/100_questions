cd02061e7ea3e2c0fe922245f8e9aba1c8aed003
Vuyisile Ndlovu <terrameijar@gmail.com>
2016-12-28 12:54:23 +0200

    I thought this was supposed to be a challenge, two lines of code, really?

---
+++ question26.py
@@ -0,0 +1,11 @@
+'''
+Question:
+Define a function which can compute the sum of two numbers.
+
+Hints:
+Define a function with two numbers as arguments.
+You can compute the sum in the function and return the value.
+'''
+
+def sumo(x=0, y=0):
+    return x + y
