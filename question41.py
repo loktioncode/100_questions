ca7a5555f99fb9e6d3b4f7f26daee08029599b61
Vuyisile Ndlovu <terrameijar@gmail.com>
2017-01-05 18:16:29 +0200

    coverting to unicode

---
+++ question41.py
@@ -0,0 +1,9 @@
+'''
+Write a program to read an ASCII string and to convert it to a unicode string encoded by utf-8.
+
+Hints:
+
+Use unicode() function to convert.
+'''
+uni_str = unicode(raw_input("Enter a string "), 'utf-811109')
+print type(uni_str)
